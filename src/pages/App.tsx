264cc6230aa48f00e1aa4a7b03215e1fb004f090
Sally <specialguest@live.co.kr>
2021-06-07 02:35:41 +0900

    test: gh-pages

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,27 +2,35 @@ import React, { lazy, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { EnhancedStore } from '@reduxjs/toolkit';
+import { CircularProgress } from '@material-ui/core';
 
 import Layout from '~components/Layout';
+import HomeContainer from '~containers/HomeContainer';
 
-const HomeContainer = lazy(() => import('~containers/HomeContainer'));
+// const HomeContainer = lazy(() => import('~containers/HomeContainer'));
 
 interface AppProps {
   store: EnhancedStore;
 }
 
+// const App = ({ store }: AppProps) => (
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <Layout>
+//         <Suspense fallback={<CircularProgress />}>
+//           <Switch>
+//             <Route path="/" component={HomeContainer} exact />
+//           </Switch>
+//         </Suspense>
+//       </Layout>
+//     </BrowserRouter>
+//   </Provider>
+// );
+
 const App = ({ store }: AppProps) => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Layout>
-        <Suspense fallback={null}>
-          <Switch>
-            <Route path="/" component={HomeContainer} exact />
-          </Switch>
-        </Suspense>
-      </Layout>
-    </BrowserRouter>
-  </Provider>
+  <Layout>
+    <HomeContainer />
+  </Layout>
 );
 
 export default App;
